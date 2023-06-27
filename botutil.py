a770c62c6b0448ee61abb29d54ed640f4e5fe4e8
okadapy <ozzajilon@gmail.com>
2023-06-27 14:42:23 +0300

    Сделал че то

--- botutil.py
+++ botutil.py
@@ -1,18 +1,26 @@
-
 from sqlite3 import connect
 from time import ctime, time
 import enum
+from telebot import TeleBot
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 class StatesEnum(enum.IntEnum):
     Start = 0
     Photos = 1
     Text = 2
+    SendToGoup = 10
+    DeleteFromSugg = 11
     Exit = 999
 
 
 class Suggestion(object):
-    def __init__(self, text=None, uid=None, photos=None, date=ctime(time()), posted=False):
+    sugg_chat = -881724072
+    group_id = -1001912688972
+
+    def __init__(
+            self, text=None, uid=None, photos=None, date=ctime(time()), posted=False
+    ):
         if photos is None:
             photos = list()
         self.text = text
@@ -22,6 +30,7 @@ class Suggestion(object):
         self.posted = posted
 
     def photos_str(self):
+        """Строка фотографий на основе списка для записи в БД"""
         if len(self.photos) == 0:
             return ""
         s = self.photos[0]
@@ -31,22 +40,44 @@ class Suggestion(object):
         return s
 
     def write(self):
+        """Запись предложки в БД"""
+        # TODO! Отлетает из за OUTPUT, а мне хочется все же айдишник предложи вытащить, что бы ее можно было постить.
+        # Если есть мысли как это сделать иначе - пиши.
         con = connect("master.db")
-        con.cursor().execute(
+        res = con.cursor().execute(
             f"INSERT OR IGNORE INTO suggestions "
-            f"(text, uid, photos, date, posted) "
+            f"(text, uid, photos, date, posted) OUTPUT INSERTED.id"
             f"VALUES ('{self.text}', {self.uid}, '{self.photos_str()}', '{self.date}', {self.posted})"
-        )
+        ).fetchone()
+        self.id = res[0]
         con.commit()
         con.close()
 
-
+    def send_to_redactors(self, bot: TeleBot):
+        """Отправка в конфу редачей, работает через жопу"""
+        # TODO!
+        for j in self.photos:
+            bot.send_photo(photo=j, chat_id=self.sugg_chat)
+        buttons = (
+            InlineKeyboardButton("Отправить", callback_data={"state": StatesEnum.SendToGoup, "id": self.id}),
+            InlineKeyboardButton("Удалить", callback_data={"state": StatesEnum.DeleteFromSugg, "id": self.id})
+        )
+        bot.send_message(
+            self.sugg_chat, self.text, reply_markup=InlineKeyboardMarkup().add(*buttons)
+        )
 
 
 def read_suggestion(post_id) -> Suggestion:
+    """Выгрузка саджа из предложки, не проверялась"""
     con = connect("master.db")
-    res = con.cursor().execute(f"SELECT * FROM suggestions WHERE id = {post_id}").fetchone()
-    return Suggestion(res[1], int(res[2]), res[3], res[4], True if "True" == res[5] else False)
+    res = (
+        con.cursor()
+        .execute(f"SELECT * FROM suggestions WHERE id = {post_id}")
+        .fetchone()
+    )
+    return Suggestion(
+        res[1], int(res[2]), res[3], res[4], True if "True" == res[5] else False
+    )
 
 
 def init_db():
@@ -56,24 +87,26 @@ def init_db():
         "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, "
         "uid LONG NOT NULL UNIQUE,"
         "uname TEXT NOT NULL,"
-        "date TEXT);"
+        "date TEXT NOT NULL,"
+        "is_admin BOOL NOT NULL);"
     )
     cur.execute(
         "CREATE TABLE IF NOT EXISTS suggestions(id INTEGER PRIMARY KEY,"
         "text TEXT,"
         "uid LONG NOT NULL,"
         "photos TEXT NOT NULL,"
-        "date TEXT,"
+        "date TEXT NOT NULL,"
         "posted BOOL NOT NULL);"
     )
     con.commit()
     con.close()
 
 
-def create_user(uid: int, uname: str):
+def create_user(uid: int, uname: str, is_admin=False):
+    """Создание нью пользователя"""
     con = connect("master.db")
     con.cursor().execute(
-        f"INSERT OR IGNORE INTO users (uid, uname, date) VALUES ({uid}, '{uname}', '{ctime(time())}');"
+        f"INSERT OR IGNORE INTO users (uid, uname, date, is_admin) VALUES ({uid}, '{uname}', '{ctime(time())}', {is_admin});"
     )
     con.commit()
     con.close()
--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from telebot import TeleBot
 from telebot.types import *
 from botutil import *
 
+sugg_chat = -881724072
+group_id = -1001912688972
 settings = {
-    'TOKEN': "6150607867:AAH1kQ_8HqYPfMG4xW19MHtwkowxLFFfkQI",
+    "TOKEN": "6170839787:AAEK8E_BWQyuq7i-FAfrfeDJ4B_GvHmCwq8",
 }
 
-bot = TeleBot(settings['TOKEN'])
+bot = TeleBot(settings["TOKEN"])
+me = bot.get_me()
+print(f"Logged in as\n{me.first_name}\n{me.id}\n@{me.username}")
 init_db()
 
 
@@ -14,9 +18,9 @@ def render_markup(state: StatesEnum):
     if state == StatesEnum.Start:
         buttons = (KeyboardButton("Фото"), KeyboardButton("Текст"))
     elif state == StatesEnum.Photos:
-        buttons = (KeyboardButton("Текст"))
+        buttons = KeyboardButton("Текст")
     elif state == StatesEnum.Text:
-        buttons = (KeyboardButton("Фото"))
+        buttons = KeyboardButton("Фото")
     else:
         buttons = None
 
@@ -25,23 +29,40 @@ def render_markup(state: StatesEnum):
 
 @bot.message_handler(commands=["start"])
 def index(message: Message):
-    create_user(message.from_user.id, message.from_user.username)
-    bot.reply_to(message, 'Привет! &#128075;\nДля того что бы отправить пост в предложку\nзаполните все пункты',
-                 reply_markup=render_markup(StatesEnum.Start),
-                 parse_mode="HTML")
+    """Основной хендлер чата, начинается с комманды /start"""
+    create_user(message.from_user.id, message.from_user.username, is_admin=False)
+    bot.reply_to(
+        message,
+        "Привет! &#128075;\nДля того что бы отправить пост в предложку\nзаполните все пункты",
+        reply_markup=render_markup(StatesEnum.Start),
+        parse_mode="HTML",
+    )
     suggestion = Suggestion()
     suggestion.uid = message.from_user.id
     bot.register_next_step_handler(message, states_logic, suggestion=suggestion)
 
+
+@bot.callback_query_handler(func=lambda call: True)
+def answer(call: CallbackQuery):
+    """Отпрака в группу постов прошедших модерацию WIP"""
+    # TODO!
+    if call == StatesEnum.SendToGoup:
+        bot.send_message(group_id, call.message.text)
+
+
 def states_logic(message: Message, suggestion: Suggestion):
+    """Не самая полезная штука, проверяет куда отправить некст сообщения"""
     if message.text == "Фото":
-        bot.reply_to(message, "Отлично!\nМаксимум - 10 фотографий, не забывайте об этом")
+        bot.reply_to(
+            message, "Отлично!\nМаксимум - 10 фотографий, не забывайте об этом"
+        )
         bot.register_next_step_handler(message, parse_photos, suggestion)
     elif message.text == "Текст":
         bot.reply_to(message, "Отлично!\nТогда пришлите текст следующим сообщением!")
         bot.register_next_step_handler(message, parse_photos, suggestion=suggestion)
     else:
         bot.reply_to(message, "Ну и ладно, будем ждать вас снова(")
+
 
 # На самом деле мой код тоже говно
 # Если хочешь сделать заебись -- сделай нахуй 2 отдельных класса: Suggestion и SuggestionBuilder
@@ -52,26 +73,32 @@ def states_logic(message: Message, suggestion: Suggestion):
 # Он будет создаваться пустым, меняться в этом боте и у него будет функция build_suggestion, которая создат уже
 # мьютабл Suggestion в конце. Будет збс
 
+
 def parse_photos(message: Message, suggestion: Suggestion):
-     if message.content_type == "photo" and len(suggestion.photos) < 10:
-         suggestion.photos.append(message.photo[-1].file_id)
-         bot.reply_to(message, "Записал!")
-         bot.register_next_step_handler(message, parse_photos, suggestion=suggestion)
-     elif suggestion.text is None and message.content_type == "text":
-         bot.reply_to(message, "Ну, видимо, хватит :D\nТеперь текст!")
-         bot.register_next_step_handler(message, parse_text, suggestion=suggestion)
-     else:
-         suggestion.write()
-         bot.reply_to(message, f"Всё пиздато\n'{suggestion.text}'\n'{suggestion.photos_str()}'")
+    """Парсинг фото. Надо попробовать сделать так, что бы он мог парсить еще и кучу фоток в одном соо"""
+    # TODO!
+    if message.content_type == "photo" and len(suggestion.photos) < 10:
+        suggestion.photos.append(message.photo[-1].file_id)
+        bot.reply_to(message, "Записал!")
+        bot.register_next_step_handler(message, parse_photos, suggestion=suggestion)
+    elif suggestion.text is None and message.content_type == "text":
+        bot.reply_to(message, "Ну, видимо, хватит :D\nТеперь текст!")
+        bot.register_next_step_handler(message, parse_text, suggestion=suggestion)
+    else:
+        suggestion.write()
+        suggestion.send_to_redactors()
+        bot.reply_to(message, f"Отлично, пост отправлен на обработку!")
+
 
 def parse_text(message: Message, suggestion: Suggestion):
-     suggestion.text = message.text
-     if len(suggestion.photos) == 0:
-         bot.reply_to(message, "Отлично, а теперь фото!")
-         bot.register_next_step_handler(message, parse_photos, suggestioт=suggestion)
-     else:
-         suggestion.write()
-         bot.reply_to(message, f"Всё пиздато\n'{suggestion.text}'\n'{suggestion.photos_str()}'")
+    suggestion.text = message.text
+    if len(suggestion.photos) == 0:
+        bot.reply_to(message, "Отлично, а теперь фото!")
+        bot.register_next_step_handler(message, parse_photos, suggestioт=suggestion)
+    else:
+        suggestion.write()
+        suggestion.send_to_redactors(bot)
+        bot.reply_to(message, f"Отлично, пост отправлен на обработку!")
 
 
 bot.infinity_polling()
